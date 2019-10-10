9e15d2507e860259aea63cd09bd7b1f76898dd75
Joao Moraes <joaomoraesbr@gmail.com>
2019-10-10 18:54:48 -0300

    updates

---
+++ build/js/resources.js
@@ -0,0 +1,47 @@
+game.resources = [
+    {
+        "name": "cacti",
+        "type": "image",
+        "src": "data/img/cacti.png"
+    },
+    {
+        "name": "cloud1",
+        "type": "image",
+        "src": "data/img/cloud1.png"
+    },
+    {
+        "name": "cloud2",
+        "type": "image",
+        "src": "data/img/cloud2.png"
+    },
+    {
+        "name": "cloud3",
+        "type": "image",
+        "src": "data/img/cloud3.png"
+    },
+    {
+        "name": "enemyCacti",
+        "type": "image",
+        "src": "data/img/enemyCacti.png"
+    },
+    {
+        "name": "enemyFly",
+        "type": "image",
+        "src": "data/img/enemyFly.png"
+    },
+    {
+        "name": "plant1",
+        "type": "image",
+        "src": "data/img/plant1.png"
+    },
+    {
+        "name": "player",
+        "type": "image",
+        "src": "data/img/player.png"
+    },
+    {
+        "name": "sidewalk",
+        "type": "image",
+        "src": "data/img/sidewalk.png"
+    }
+];
